6bdb778a536a61c85e7452c1f4ed6f19c5f92c3e
ashrafkamarudin <ashrafkamarudin1995@gmail.com>
2019-10-09 14:24:02 +0800

    [Delete] Removed api key

--- test/http_api_call_test.dart
+++ test/http_api_call_test.dart
@@ -4,22 +4,20 @@ import 'package:http_api_call/http_api_call.dart';
 void main() {
   test('Test Auth', () async {
 
-    // ApiResponse response = await Api(
-    //   apiKey: 'k6pWb106e34MN8jjLyPWO5rYrtMRzWbw9UYL9FtKJwAF',
-    //   secretKey: 'J694AAqcMADTPaVH427zpI2DwYg6zZwH32ewcTAloZQRK7l1gxG6DkNh3TGlo10C')
-    // .get("https://manage.runcloud.io/api/v2/ping")
-    // .then((response) {
-    //   return ApiResponse.fromJson(json.decode(response.body));
-    // });
+    await Api( // get method
+      apiKey: 'Username',
+      secretKey: 'J694AAqcMADTPaVH427zpI2DwYg6zZwH32ewcTAloZQRK7l1gxG6DkNh3TGlo10C')
+    .get("https://manage.runcloud.io/api/v2/ping");
 
-    await Api(
-      apiKey: 'k6pWb106e34MN8jjLyPWO5rYrtMRzWbw9UYL9FtKJwAF',
+    await Api( // post method
+      apiKey: 'Password',
       secretKey: 'J694AAqcMADTPaVH427zpI2DwYg6zZwH32ewcTAloZQRK7l1gxG6DkNh3TGlo10C')
     .setBody({
       "name" : "test api 2",
       "ipAddress" : "192.168.0.2",
       "provider": "Custom"
     }).post('https://manage.runcloud.io/api/v2/servers');
+
     // expect(rc.auth, 'test');
   });
 }
